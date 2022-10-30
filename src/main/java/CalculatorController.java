b7c066298e10e2c051d4a7d65dbfb75dd5276bd6
Pronyushkin_qa <kollaydereva@gmail.com>
2022-10-30 15:32:04 +0300

    1

---
+++ src/main/java/CalculatorController.java
@@ -0,0 +1,38 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class CalculatorController {
+    private final CalculatorService calculatorService;
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+    @GetMapping(path = "/calculator")
+    public String hello() {
+        return "<span><b>Welcome to Calculator!</b><span><br>";
+    }
+
+    @GetMapping(path = "/calculator/plus")
+    public String plus(@RequestParam("num1") int num1,
+                       @RequestParam("num2") int num2) {
+        return num1 + " + " + num2 + " = " + calculatorService.plus(num1, num2);
+    }
+
+    @GetMapping(path = "/calculator/minus")
+    public String minus(@RequestParam("num1") int num1,
+                        @RequestParam("num2") int num2) {
+        return num1 + " - " + num2 + " = " + calculatorService.minus(num1, num2);
+    }
+
+    @GetMapping(path = "/calculator/multiply")
+    public String multiply(@RequestParam("num1") int num1,
+                           @RequestParam("num2") int num2) {
+        return num1 + " * " + num2 + " = " + calculatorService.multiply(num1, num2);
+    }
+
+    @GetMapping(path = "/calculator/divide")
+    public String divide(@RequestParam("num1") int num1,
+                         @RequestParam("num2") int num2) {
+        return num1 + " / " + num2 + " = " + calculatorService.divide(num1, num2);
+    }
+}
